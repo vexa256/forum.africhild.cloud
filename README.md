3009847a30e81088f14df9e717acdd0af868ea94
Ayebare Timothy <vexa256@gmail.com>
2022-06-22 13:55:09 +0300

    V2 Update - Initial 0.203

---
+++ README.md
@@ -0,0 +1,4 @@
+This is the AfriChild Center's Forum Software.
+
+"Building The next generation of child- focused researchers"
+The AfriChild Centre exists to generate research that informs policy and practice for the wellbeing of children.
---
+++ config.php
@@ -0,0 +1,29 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => 3306,
+    'database' => 'africhild.forum',
+    'username' => 'hacker',
+    'password' => 'kamukama',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+    'engine' => NULL,
+    'prefix_indexes' => true,
+  ),
+  'url' => 'http://africhild.forum',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+  'headers' => 
+  array (
+    'poweredByHeader' => true,
+    'referrerPolicy' => 'same-origin',
+  ),
+);
